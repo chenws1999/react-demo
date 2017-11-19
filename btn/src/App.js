b5f50a80d928c86006caba8a502d49ce69906144
chenws <chenws199@gmail.com>
2017-11-19 23:59:19 +0800

    add loading

--- btn/src/App.js
+++ btn/src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 import Btn from './Btn/Btn.js';
 import RaisedBtn from './RaisedBtn/RaisedBtn.js';
 import BtnTabs from './BtnTabs/BtnTabs.js';
+import Button from './button.js';
+function res () {
+  return new Promise(res => {
+    setTimeout(_ => {
+      res();
+    }, 1000);
+  });
+}
 
 class App extends Component {
   render() {
@@ -14,8 +22,10 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <Btn label="test" status={1} type={0}/>
-        <RaisedBtn onClick={_ => console.log('test')}/>
+        <Btn label="test" status={0} type={0}/>
+        <RaisedBtn onClick={res}/>
         <BtnTabs labels={[1, 3]}/>
+        <Button />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- btn/src/button.js
+++ btn/src/button.js
@@ -1,7 +1,74 @@
 import React from 'react';
+import './style.css';
 
 class Button extends React.Component {
     constructor (props) {
-        this.super(props);
+        super(props);
+        this.props = Object.assign({
+            preCheck () {
+                return true;
+            },
+            onClick () {
+                console.log('button click');
+            },
+            disabled: false,
+            type: 0,
+            label: '',
+            size: 'medium',
+        }, props);
+        this.state = {
+            loading: true
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    render () {
+        // if ()
+        return (
+            <button type="button" className="button">
+                <div className="item">
+                    {this.state.loading && <LoadEffect/>}
+                </div>
+                <div className="item">
+                </div>
+            </button>
+        )
+    }
+    handleClick () {
+        if (this.state.loading)
+            return;
+        let res = this.props.onClick();
+        if (res instanceof Promise) {
+            this.setState({
+                loading: true
+            });
+            res.then(res => {
+                this.setState({
+                    loading: false
+                })
+            }).catch(e => {
+                this.setState({
+                    loading: false
+                })
+                console.log('error: ', e);
+            });
+            return;
+        }
     }
 }
+
+function LoadEffect () {
+    return (
+        <div className="loadEffect">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
+
+export default Button;
